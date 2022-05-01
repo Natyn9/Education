cb315b9277e95edd3042948fbf2c38cdeaac0797
Наталья Рузаева <natyn9@mail.ru>
2022-05-01 15:29:07 +0200

    New files

--- JS. HW_2.js
+++ JS. HW_2.js
@@ -1,29 +1,61 @@
+// function login(check){
+//   // let counter 
+//     if (typeof check == 'string') {
+//       if (check.length <= 4){
+//         console.log("Short name. Enter from 5 characters")
+//       } else if (check.length >= 65){
+//         console.log("Too long name")}
+//         else if (!check.match (/[a-zа-яё]/i) ){
+//           console.log("Enter letters")
+//         }
+//         else if (!check.match (/[A-ZА-ЯЁ]/)) {
+//           console.log("Enter Bigletters")
+//         }
+//         else if (!check.match (/[0-9]/) ){
+//           console.log("Enter numbers")
+//         }
+//         else if (!check.match(/[@]/)){
+//           console.log("Enter @")
+//         }
+//         else {
+//           console.log("Thancks, it's ok")
+//         }
+//     }
+//     else {
+//       console.log("Enter string")
+//     } 
+// }
+  
+//   login("")
+
+
+
 function login(check){
   // let counter 
     if (typeof check == 'string') {
       if (check.length <= 4){
-        console.log("Short name. Enter from 5 characters")
+        alert("Short name. Enter from 5 characters")
       } else if (check.length >= 65){
-        console.log("Too long name")}
+        alert("Too long name")}
         else if (!check.match (/[a-zа-яё]/i) ){
-          console.log("Enter letters")
+            alert("Enter letters")
         }
         else if (!check.match (/[A-ZА-ЯЁ]/)) {
-          console.log("Enter Bigletters")
+            alert("Enter Bigletters")
         }
         else if (!check.match (/[0-9]/) ){
-          console.log("Enter numbers")
+            alert("Enter numbers")
         }
         else if (!check.match(/[@]/)){
-          console.log("Enter @")
+            alert("Enter @")
         }
         else {
-          console.log("Thancks, it's ok")
+            alert("Thancks, it's ok")
         }
     }
     else {
-      console.log("Enter string")
+        alert("Enter string")
     } 
 }
   
-  login("")
+login (prompt ("Enter login", ""))
