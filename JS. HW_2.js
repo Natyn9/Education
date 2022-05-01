e009e68126125744fb6a819e3b728984313f074a
Наталья Рузаева <natyn9@mail.ru>
2022-05-01 14:58:16 +0200

    New fail

---
+++ JS. HW_2.js
@@ -0,0 +1,29 @@
+function login(check){
+  // let counter 
+    if (typeof check == 'string') {
+      if (check.length <= 4){
+        console.log("Short name. Enter from 5 characters")
+      } else if (check.length >= 65){
+        console.log("Too long name")}
+        else if (!check.match (/[a-zа-яё]/i) ){
+          console.log("Enter letters")
+        }
+        else if (!check.match (/[A-ZА-ЯЁ]/)) {
+          console.log("Enter Bigletters")
+        }
+        else if (!check.match (/[0-9]/) ){
+          console.log("Enter numbers")
+        }
+        else if (!check.match(/[@]/)){
+          console.log("Enter @")
+        }
+        else {
+          console.log("Thancks, it's ok")
+        }
+    }
+    else {
+      console.log("Enter string")
+    } 
+}
+  
+  login("")
